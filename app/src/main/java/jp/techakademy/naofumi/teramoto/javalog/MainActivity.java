b6b9450b65fc6d1e4bcba9d27c5f7d44fa10229c
NaofumiTeramoto <nfteramoto@gmail.com>
2016-12-26 21:02:34 +0900

    Initial commit

---
+++ app/src/main/java/jp/techakademy/naofumi/teramoto/javalog/MainActivity.java
@@ -0,0 +1,26 @@
+package jp.techakademy.naofumi.teramoto.javalog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int num = 60;
+
+        if (num >= 90) {
+            Log.d("javatest", "優");
+        } else if (num >= 75) {
+            Log.d("javatest", "良");
+        } else if (num >= 60) {
+            Log.d("javatest", "可");
+        } else {
+            Log.d("javatest", "不可");
+        }
+    }
+}
